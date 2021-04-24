558f87b8a7726e423b439710718ca8b8512f7aab
mnpkoerner <mnpkoerner@gmail.com>
2021-04-23 22:03:49 -0500

    some logic to manage search results in the case of no matching results

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -2,33 +2,43 @@ import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 
 
-export default function Results () {
+export default function Results() {
 
     const rhymeReturn = useSelector(store => store.words.rhyme)
     const synonymReturn = useSelector(store => store.words.synonym)
 
     const [overlap, setOverlap] = useState('')
 
-    const compare = (arrayOne, arrayTwo) =>{
-        const intersections = arrayOne.filter(e => arrayTwo.indexOf(e) !== -1)
-        console.log('intersections is', intersections)
-        return intersections;
+    //if there is
+    const compare = (arrayOne, arrayTwo) => {
+        console.error('in compare')
+        let venn;
+        if (arrayOne && arrayTwo) {
+            venn = arrayOne.filter(e => arrayTwo.indexOf(e) !== -1)
+        }
+        if(!venn[0]){
+            venn = arrayTwo;
+            venn.unshift('No results matched your search, instead some synonyms')
+        }
+        console.error('before return', venn)
+        return venn;
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         const shared = compare(rhymeReturn.rhymes.all, synonymReturn.synonyms)
         setOverlap(shared)
     }, [])
-    return(
+
+    return (
         <>
-        <h1>THERE BE STUFF</h1>
-        <ul>
-        {overlap ? overlap.map((word)=>{
-            return(
-                <li>{word}</li>
-            )
-        }) : <span></span>}
-        </ul>
+            <h1>THERE BE STUFF</h1>
+            <ul>
+                {overlap ? overlap.map((word) => {
+                    return (
+                        <li>{word}</li>
+                    )
+                }) : <span></span>}
+            </ul>
         </>
     )
 }
