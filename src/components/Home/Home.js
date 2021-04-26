5090901821ea3de20a04af73c4eaebfd301f5e28
mnpkoerner <mnpkoerner@gmail.com>
2021-04-25 23:41:12 -0500

    imported font

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,17 @@
-import {useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 export default function Home() {
 
     const history = useHistory()
 
-    return(
+    return (
         <>
-        <header><h1>Placeholder for Chatterbox image</h1></header>
-        <main>
-            <p>Description</p>
-            <button
-                onClick={()=>history.push('/words')}
-            >Try It Out!</button>
+            <header><h1>Placeholder for Chatterbox image</h1></header>
+            <main>
+                <p>Longwinded description of what this thing is supposed to do</p>
+                <button
+                    onClick={() => history.push('/words')}
+                >Try It Out!</button>
             </main>
         </>
     )
